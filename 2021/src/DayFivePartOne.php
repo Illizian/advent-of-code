90638da11417df36848a03a41d6a4b768b09a554
Alex Scotton <alex@alexscotton.com>
2021-12-08 13:07:38 +0000

    Complete DayFive (Part Two Broken)

---
+++ 2021/src/DayFivePartOne.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Illizian\AdventOfCode;
+
+use Illuminate\Support\Collection;
+
+class DayFivePartOne
+{ 
+    public static function process(array $input): int
+    {
+        return Collection::make(
+            array_map(
+                fn($row) => array_map(fn($coords) => explode(',', $coords), explode(' -> ', $row)),
+                $input
+            )
+        )
+            ->filter(fn($coords) => (
+                // If x1 != x2 || y1 != y2, then remove
+                $coords[0][0] === $coords[1][0] ||
+                $coords[0][1] === $coords[1][1]
+            ))
+            ->map(function($coords) {
+                // Direction: 0 = X // 1 = Y
+                $dir = +($coords[0][0] === $coords[1][0]);
+                $length = abs($coords[0][$dir] - $coords[1][$dir]) + 1;
+                if ($length <= 2) return $coords;
+                
+                $start = intval($coords[+($coords[0][$dir] > $coords[1][$dir])][$dir]);
+                $fill = array_fill(0, $length, null);
+
+                return array_map(
+                    fn($key) => [
+                        ($dir === 0) ? $start + $key : intval($coords[0][0]),
+                        ($dir === 1) ? $start + $key : intval($coords[0][1]),
+                    ],
+                    array_keys($fill),
+                    $fill
+                );
+            })
+            ->flatten(1)
+            ->map(fn($coords) => "{$coords[0]} x {$coords[1]}")
+            ->groupBy(fn($str) => $str)
+            ->map
+            ->count()
+            ->filter(fn($val) => $val > 1)
+            ->count();
+    }
+}
---
+++ 2021/src/DayFivePartTwo.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Illizian\AdventOfCode;
+
+use Illuminate\Support\Collection;
+
+class DayFivePartTwo
+{
+    public static function process(array $input): int
+    {
+        return Collection::make(
+            array_map(
+                fn($row) => array_map(fn($coords) => explode(',', $coords), explode(' -> ', $row)),
+                $input
+            )
+        )
+            ->map(function($coords) {
+                [ [ $x1, $y1 ], [ $x2, $y2 ] ] = $coords;
+                $dir = [
+                    ($x1 === $x2) ? 0 : ($x1 > $x2 ? -1 : 1),
+                    ($y1 === $y2) ? 0 : ($y1 > $y2 ? -1 : 1)
+                ];
+                $len = 1 + intval(
+                    sqrt(
+                        pow($x2 - $x1, 2) + pow($y2 - $y1, 2)
+                    )
+                );
+                $fill = array_fill(0, $len, null);
+
+                return array_map(
+                    fn($key) => [
+                        intval($x1) + ($key * $dir[0]),
+                        intval($y1) + ($key * $dir[1]),
+                    ],
+                    array_keys($fill),
+                    $fill
+                );
+            })
+            ->flatten(1)
+            ->map(fn($coords) => "{$coords[0]} x {$coords[1]}")
+            ->groupBy(fn($str) => $str)
+            ->map
+            ->count()
+            ->filter(fn($val) => $val > 1)
+            ->count();
+    }
+}
