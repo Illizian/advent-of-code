1f248da8915c58f371fffe7b9269b93549852f6d
Alex Scotton <alex@alexscotton.com>
2022-12-01 11:18:41 +0000

    DayOne: PartTwo Results

--- 2022/src/DayOne/DayOne.php
+++ 2022/src/DayOne/DayOne.php
@@ -12,4 +12,14 @@ class DayOne
       ->sortDesc()
       ->first();
   }
+
+  public static function PartTwo(string $input): int
+  {
+    return collect(explode("\n\n", $input))
+      ->map(fn (string $list) => explode("\n", $list))
+      ->map(fn (array $list) => array_reduce($list, fn (int $total, string $item) => $total + (int) $item, 0))
+      ->sortDesc()
+      ->take(3)
+      ->reduce(fn (int $total, int $calories) => $total + $calories, 0);
+  }
 }
--- 2022/tests/DayOne/DayOneTest.php
+++ 2022/tests/DayOne/DayOneTest.php
@@ -12,3 +12,14 @@ it('processes the Part One input, and provides the expected output', function ($
   ['/input-example.txt', 24000],
   ['/input-part1.txt', 69281],
 ]);
+
+it('processes the Part Two input, and provides the expected output', function ($file, $expected) {
+  $input = file_get_contents(__DIR__ . $file);
+
+  $output = DayOne::PartTwo($input);
+
+  expect($output)->toEqual($expected);
+})->with([
+  ['/input-example.txt', 45000],
+  ['/input-part1.txt', 201524],
+]);
