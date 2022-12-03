a64ff7c4bec291fd612ed5e1b9fc356b9bc1cbd5
Alex Scotton <alex@alexscotton.com>
2022-12-03 18:59:39 +0000

    DayThree: PartTwo Results

--- 2022/src/DayThree/DayThree.php
+++ 2022/src/DayThree/DayThree.php
@@ -2,6 +2,8 @@
 
 namespace Illizian\AdventOfCode2022\DayThree;
 
+use Illuminate\Support\Collection;
+
 class DayThree
 {
   public static function PartOne(string $input): int
@@ -21,6 +23,27 @@ class DayThree
       })
       ->filter()
       ->map(fn (string $duplicate) => array_search($duplicate, $values))
+      ->sum();
+  }
+
+  public static function PartTwo(string $input): int
+  {
+    $values = [null, ...range('a', 'z'), ...range('A', 'Z')];
+
+    return collect(explode("\n", $input))
+      ->chunk(3)
+      ->map(fn (Collection $group) => $group->map(fn (string $backpack) => collect(str_split($backpack, 1))))
+      ->map(function (Collection $group) {
+        [$first, $second, $third] = $group->values();
+
+        return $first
+          ->filter(fn ($item) => $second->contains($item))
+          ->filter(fn ($item) => $third->contains($item))
+          ->first();
+        return $group;
+      })
+      ->filter()
+      ->map(fn (string $badge) => array_search($badge, $values))
       ->sum();
   }
 }
--- 2022/tests/DayThree/DayThreeTest.php
+++ 2022/tests/DayThree/DayThreeTest.php
@@ -12,3 +12,14 @@ it('processes the Part One input, and provides the expected output', function ($
   ['/input-example.txt', 157],
   ['/input-part1.txt', 7793],
 ]);
+
+it('processes the Part Two input, and provides the expected output', function ($file, $expected) {
+  $input = file_get_contents(__DIR__ . $file);
+
+  $output = DayThree::PartTwo($input);
+
+  expect($output)->toEqual($expected);
+})->with([
+  ['/input-example.txt', 70],
+  ['/input-part1.txt', 2499],
+]);
