328956fb41a8a8f1adcac8095153f06ee104d3f2
Alex Scotton <alex@alexscotton.com>
2022-12-04 11:11:52 +0000

    DayFour: PartTwo Results

--- 2022/src/DayFour/DayFour.php
+++ 2022/src/DayFour/DayFour.php
@@ -23,4 +23,24 @@ class DayFour
       })
       ->count();
   }
+
+  public static function PartTwo(string $input): int
+  {
+    return collect(explode("\n", $input))
+      ->map(fn (string $instructions) => explode(",", $instructions))
+      ->map(fn (array $assignments) => array_map(fn (string $assignment) => explode('-', $assignment), $assignments))
+      ->filter(function (array $assignments) {
+        [$one, $two] = $assignments;
+        [$one_lower, $one_upper] = $one;
+        [$two_lower, $two_upper] = $two;
+
+        return (
+          ($one_lower >= $two_lower && $one_lower <= $two_upper) ||
+          ($one_upper >= $two_lower && $one_upper <= $two_upper) ||
+          ($two_lower >= $one_lower && $two_lower <= $one_upper) ||
+          ($two_upper >= $one_lower && $two_upper <= $one_upper)
+        );
+      })
+      ->count();
+  }
 }
--- 2022/tests/DayFour/DayFourTest.php
+++ 2022/tests/DayFour/DayFourTest.php
@@ -12,3 +12,14 @@ it('processes the Part One input, and provides the expected output', function ($
   ['/input-example.txt', 2],
   ['/input-part1.txt', 494],
 ]);
+
+it('processes the Part Two input, and provides the expected output', function ($file, $expected) {
+  $input = file_get_contents(__DIR__ . $file);
+
+  $output = DayFour::PartTwo($input);
+
+  expect($output)->toEqual($expected);
+})->with([
+  ['/input-example.txt', 4],
+  ['/input-part1.txt', 833],
+]);
